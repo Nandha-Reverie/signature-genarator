f518851fd69278f13c7007731d03b2c46ca05ff2
Nandhakumar <reverie-pc@Reverie-PCs-MacBook-Pro.local>
2019-08-14 13:49:13 +0530

    Changes

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,15 @@ router.post('/create', (req, res) => {
     const lname = req.body['lname']
     const designation = req.body['designation']
     const email = req.body['email']
-    const email1 = req.body['email1']
+  
     const mobile = req.body['mobile']
 
     res.render('signature', {
         fname,
         lname,
-        email,
+
         designation,
-        email1,
+        email,
         mobile
     })
 })
