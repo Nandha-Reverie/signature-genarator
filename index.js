f7817987c25fbcc5d21350d45159b77f86f9f36a
Nandhakumar <reverie-pc@Reverie-PCs-MacBook-Pro.local>
2019-09-10 15:00:33 +0530

    Changed the port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 
-const port = 5002
+const port = 3002
 const router = require('./routes/index')
 
 app.set('views', path.join(__dirname, 'views'));
